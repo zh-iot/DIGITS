faa7fca5ab02aba194c5ef91cb771eaeddf0ec1a
wan9dj <wangdongjun1@hotmail.com>
2021-07-16 02:16:55 -0700

    测试处理form表单翻译

--- digits/model/images/forms.py
+++ digits/model/images/forms.py
@@ -5,7 +5,7 @@ from wtforms import validators
 
 from ..forms import ModelForm
 from digits import utils
-from flask_babel import Babel, gettext as _
+from flask_babel import Babel, gettext as _, lazy_gettext
 
 
 
@@ -15,118 +15,118 @@ class ImageModelForm(ModelForm):
     """
 
     crop_size = utils.forms.IntegerField(
-        _('Crop Size'),
+        lazy_gettext('Crop Size'),
         validators=[
             validators.NumberRange(min=1),
             validators.Optional()
         ],
-        tooltip=_("If specified, during training a random square crop will be taken from the input image before using as input for the network.")
+        tooltip=lazy_gettext("If specified, during training a random square crop will be taken from the input image before using as input for the network.")
     )
 
     use_mean = utils.forms.SelectField(
-        _('Subtract Mean'),
+        lazy_gettext('Subtract Mean'),
         choices=[
-            ('none', _('None')),
-            ('image', _('Image')),
-            ('pixel', _('Pixel')),
+            ('none', lazy_gettext('None')),
+            ('image', lazy_gettext('Image')),
+            ('pixel', lazy_gettext('Pixel')),
         ],
         default='image',
-        tooltip=_("Subtract the mean file or mean pixel for this dataset from each image.")
+        tooltip=lazy_gettext("Subtract the mean file or mean pixel for this dataset from each image.")
     )
 
     aug_flip = utils.forms.SelectField(
-        _('Flipping'),
+        lazy_gettext('Flipping'),
         choices=[
-            ('none', _('None')),
-            ('fliplr', _('Horizontal')),
-            ('flipud', _('Vertical')),
-            ('fliplrud', _('Horizontal and/or Vertical')),
+            ('none', lazy_gettext('None')),
+            ('fliplr', lazy_gettext('Horizontal')),
+            ('flipud', lazy_gettext('Vertical')),
+            ('fliplrud', lazy_gettext('Horizontal and/or Vertical')),
         ],
         default='none',
-        tooltip=_("Randomly flips each image during batch preprocessing.")
+        tooltip=lazy_gettext("Randomly flips each image during batch preprocessing.")
     )
 
     aug_quad_rot = utils.forms.SelectField(
-        _('Quadrilateral Rotation'),
+        lazy_gettext('Quadrilateral Rotation'),
         choices=[
-            ('none', _('None')),
-            ('rot90', _('0, 90 or 270 degrees')),
-            ('rot180', _('0 or 180 degrees')),
-            ('rotall', _('0, 90, 180 or 270 degrees.')),
+            ('none', lazy_gettext('None')),
+            ('rot90', lazy_gettext('0, 90 or 270 degrees')),
+            ('rot180', lazy_gettext('0 or 180 degrees')),
+            ('rotall', lazy_gettext('0, 90, 180 or 270 degrees.')),
         ],
         default='none',
-        tooltip=_("Randomly rotates (90 degree steps) each image during batch preprocessing.")
+        tooltip=lazy_gettext("Randomly rotates (90 degree steps) each image during batch preprocessing.")
     )
 
     aug_rot = utils.forms.IntegerField(
-        _('Rotation (+- deg)'),
+        lazy_gettext('Rotation (+- deg)'),
         default=0,
         validators=[
             validators.NumberRange(min=0, max=180)
         ],
-        tooltip=_("The uniform-random rotation angle that will be performed during batch preprocessing.")
+        tooltip=lazy_gettext("The uniform-random rotation angle that will be performed during batch preprocessing.")
     )
 
     aug_scale = utils.forms.FloatField(
-        _('Rescale (stddev)'),
+        lazy_gettext('Rescale (stddev)'),
         default=0,
         validators=[
             validators.NumberRange(min=0, max=1)
         ],
-        tooltip=_("Retaining image size, the image is rescaled with a +-stddev of this parameter. Suggested value is 0.07.")
+        tooltip=lazy_gettext("Retaining image size, the image is rescaled with a +-stddev of this parameter. Suggested value is 0.07.")
     )
 
     aug_noise = utils.forms.FloatField(
-        _('Noise (stddev)'),
+        lazy_gettext('Noise (stddev)'),
         default=0,
         validators=[
             validators.NumberRange(min=0, max=1)
         ],
-        tooltip=_("Adds AWGN (Additive White Gaussian Noise) during batch preprocessing, assuming [0 1] pixel-value range. Suggested value is 0.03.")
+        tooltip=lazy_gettext("Adds AWGN (Additive White Gaussian Noise) during batch preprocessing, assuming [0 1] pixel-value range. Suggested value is 0.03.")
     )
 
     aug_contrast = utils.forms.FloatField(
-        _('Contrast (factor)'),
+        lazy_gettext('Contrast (factor)'),
         default=0,
         validators=[
             validators.NumberRange(min=0, max=5)
         ],
-        tooltip=_("Per channel, the mean of the channel is computed and then adjusts each component x of each pixel to (x - mean) * contrast_factor + mean. The contrast_factor is picked form a random uniform distribution to yield a value between [1-contrast_factor, 1+contrast_factor]. Suggested value is 0.8.")
+        tooltip=lazy_gettext("Per channel, the mean of the channel is computed and then adjusts each component x of each pixel to (x - mean) * contrast_factor + mean. The contrast_factor is picked form a random uniform distribution to yield a value between [1-contrast_factor, 1+contrast_factor]. Suggested value is 0.8.")
     )
 
     aug_whitening = utils.forms.BooleanField(
-        _('Whitening'),
+        lazy_gettext('Whitening'),
         default=False,
         validators=[],
-        tooltip=_("Per-image whitening by subtracting its own mean, and dividing by its own standard deviation.")
+        tooltip=lazy_gettext("Per-image whitening by subtracting its own mean, and dividing by its own standard deviation.")
     )
 
     aug_hsv_use = utils.forms.BooleanField(
-        _('HSV Shifting'),
+        lazy_gettext('HSV Shifting'),
         default=False,
-        tooltip=_("Augmentation by normal-distributed random shifts in HSV color space, assuming [0 1] pixel-value range."),
+        tooltip=lazy_gettext("Augmentation by normal-distributed random shifts in HSV color space, assuming [0 1] pixel-value range."),
     )
     aug_hsv_h = utils.forms.FloatField(
-        _('Hue'),
+        lazy_gettext('Hue'),
         default=0.02,
         validators=[
             validators.NumberRange(min=0, max=0.5)
         ],
-        tooltip=_("Standard deviation of a shift that will be performed during preprocessing, assuming [0 1] pixel-value range.")
+        tooltip=lazy_gettext("Standard deviation of a shift that will be performed during preprocessing, assuming [0 1] pixel-value range.")
     )
     aug_hsv_s = utils.forms.FloatField(
-        _('Saturation'),
+        lazy_gettext('Saturation'),
         default=0.04,
         validators=[
             validators.NumberRange(min=0, max=0.5)
         ],
-        tooltip=_("Standard deviation of a shift that will be performed during preprocessing, assuming [0 1] pixel-value range.")
+        tooltip=lazy_gettext("Standard deviation of a shift that will be performed during preprocessing, assuming [0 1] pixel-value range.")
     )
     aug_hsv_v = utils.forms.FloatField(
-        _('Value'),
+        lazy_gettext('Value'),
         default=0.06,
         validators=[
             validators.NumberRange(min=0, max=0.5)
         ],
-        tooltip=_("Standard deviation of a shift that will be performed during preprocessing, assuming [0 1] pixel-value range.")
+        tooltip=lazy_gettext("Standard deviation of a shift that will be performed during preprocessing, assuming [0 1] pixel-value range.")
     )
