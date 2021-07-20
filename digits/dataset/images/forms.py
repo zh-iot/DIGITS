759aeda899d30733633b77e300f712a8cb0fa02e
wan9dj <wangdongjun1@hotmail.com>
2021-07-20 03:29:55 -0700

    完善翻译datasets/images/classification/new

--- digits/dataset/images/forms.py
+++ digits/dataset/images/forms.py
@@ -7,6 +7,7 @@ from wtforms import validators
 from ..forms import DatasetForm
 from .job import ImageDatasetJob
 from digits import utils
+from flask_babel import Babel, gettext as _, lazy_gettext
 
 
 class ImageDatasetForm(DatasetForm):
@@ -16,38 +17,38 @@ class ImageDatasetForm(DatasetForm):
     """
 
     encoding = utils.forms.SelectField(
-        'Image Encoding',
+        lazy_gettext('Image Encoding'),
         default='png',
         choices=[
-            ('none', 'None'),
-            ('png', 'PNG (lossless)'),
-            ('jpg', 'JPEG (lossy, 90% quality)'),
+            ('none', lazy_gettext('None')),
+            ('png', lazy_gettext('PNG (lossless)')),
+            ('jpg', lazy_gettext('JPEG (lossy, 90%% quality)')),
         ],
-        tooltip=('Using either of these compression formats can save disk space, '
-                 'but can also require marginally more time for training.'),
+        tooltip=lazy_gettext('Using either of these compression formats can save disk space, '
+                 'but can also require marginally more time for training.')
     )
 
     # Image resize
 
     resize_channels = utils.forms.SelectField(
-        u'Image Type',
+        lazy_gettext(u'Image Type'),
         default='3',
-        choices=[('1', 'Grayscale'), ('3', 'Color')],
-        tooltip="Color is 3-channel RGB. Grayscale is single channel monochrome."
+        choices=[('1', lazy_gettext('Grayscale')), ('3', lazy_gettext('Color'))],
+        tooltip=lazy_gettext("Color is 3-channel RGB. Grayscale is single channel monochrome.")
     )
     resize_width = wtforms.IntegerField(
-        u'Resize Width',
+        lazy_gettext(u'Resize Width'),
         default=256,
         validators=[validators.DataRequired()]
     )
     resize_height = wtforms.IntegerField(
-        u'Resize Height',
+        lazy_gettext(u'Resize Height'),
         default=256,
         validators=[validators.DataRequired()]
     )
     resize_mode = utils.forms.SelectField(
-        u'Resize Transformation',
+        lazy_gettext(u'Resize Transformation'),
         default='squash',
         choices=ImageDatasetJob.resize_mode_choices(),
-        tooltip="Options for dealing with aspect ratio changes during resize. See examples below."
+        tooltip=lazy_gettext("Options for dealing with aspect ratio changes during resize. See examples below.")
     )
